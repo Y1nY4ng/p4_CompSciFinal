05e05211fc99ec2be22aceeb156ca5540caa041c
Y1n_yng Hong <NathanYang557@gmail.com>
2024-12-12 20:04:15 +0000

    created functions

--- game.py
+++ game.py
@@ -9,22 +9,27 @@ def shuffle_word(word):
     shuffle(word)
     return ''.join(word)
 
-for i in range(5):
-    print(shuffle_word(word))
-    user_guess = input("Level 1: Try to unshuffle the word! ")
-    if user_guess == answer1:
-        print("Thats Correct!")
-        break
-    else:
-        print("Wrong, Try Again!")
+def level_1():
+    for i in range(5):
+        print(shuffle_word(word))
+        user_guess = input("Level 1: Try to unshuffle the word! ")
+        if user_guess == answer1:
+            print("Thats Correct!")
+            break
+        else:
+            print("Wrong, Try Again!")
 
-for i in range(10):
-    word = random.choice(word_list)
-    answer2 = word
-    print(shuffle_word(word))
-    user_guess = input("Level 2: Now do it in one guess! ")
-    if user_guess == answer2:
-        print("Wow! That's Correct Again!")
-        break
-    else:
-        print("Nope, Reshuffling!")
+def level_2():
+    for i in range(10):
+        word = random.choice(word_list)
+        answer2 = word
+        print(shuffle_word(word))
+        user_guess = input("Level 2: Now do it in one guess! ")
+        if user_guess == answer2:
+            print("Wow! That's Correct Again!")
+            break
+        else:
+            print("Nope, Reshuffling!")
+
+level_1()
+level_2()
