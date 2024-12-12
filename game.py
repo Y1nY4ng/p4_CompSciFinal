72cf20be1885561c11c70a76003e77525f3e5490
Y1n_yng Hong <NathanYang557@gmail.com>
2024-12-12 19:59:38 +0000

    created the code and made two levels

---
+++ game.py
@@ -0,0 +1,30 @@
+import random
+from random import shuffle
+
+word_list = ["ribbon","baseball","dogs","document","fabricated","excessive","mississippi","redundant","zebra","mario"]
+word = random.choice(word_list)
+answer1 = word
+def shuffle_word(word):
+    word = list(word)
+    shuffle(word)
+    return ''.join(word)
+
+for i in range(5):
+    print(shuffle_word(word))
+    user_guess = input("Level 1: Try to unshuffle the word! ")
+    if user_guess == answer1:
+        print("Thats Correct!")
+        break
+    else:
+        print("Wrong, Try Again!")
+
+for i in range(10):
+    word = random.choice(word_list)
+    answer2 = word
+    print(shuffle_word(word))
+    user_guess = input("Level 2: Now do it in one guess! ")
+    if user_guess == answer2:
+        print("Wow! That's Correct Again!")
+        break
+    else:
+        print("Nope, Reshuffling!")
